ffc6ca618763a0ffe0b16f7096436536d29fb5d5
JuanCamilo787 <richin12345@gmail.com>
2019-09-05 19:14:33 -0400

    adding the update method to mi crud

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -7,7 +7,7 @@ const Todo = mongoose.model('Todo');
  * @description this function create an todo item
  */
 
-module.exports.createTodo = (req, res, next)=>{
+module.exports.createTodo = (req, res)=>{
     let todo = new Todo(req.body);
     todo.save().then(doc => {
        return res.send(doc)
@@ -16,15 +16,15 @@ module.exports.createTodo = (req, res, next)=>{
     })
 }
 
-module.exports.getTodos = (req, res, next)=>{
-    Todo.find().then(doc => {
+module.exports.getTodos = (req, res)=>{
+    Todo.find(req.body).then(doc => {
        return res.send(doc)
     }).catch(err => {
         return res.send(err)
     })
 }
 
-module.exports.getTodoById = (req, res, next)=>{
+module.exports.getTodoById = (req, res)=>{
     Todo.findById(req.params.id).then(doc => {
        return res.send(doc)
     }).catch(err => {
@@ -32,10 +32,16 @@ module.exports.getTodoById = (req, res, next)=>{
     })
 }
 
-module.exports.deleteTodos = (req, res, next)=>{
-    Todo.findById(req.params.id).then(doc => {
-       return res.send(doc)
-    }).catch(err => {
-        return res.send(err)
+module.exports.deleteTodos = (req, res)=>{
+    Todo.remove(req.body, (err, doc)=>{
+        if (err) throw err;
+        res.send(doc);
+    })
+}
+
+module.exports.updateTodos = (req, res)=>{
+    Todo.update(req.body[0], req.body[1], (err, doc)=>{
+        if (err) throw err;
+        res.send(doc);
     })
 }
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -6,5 +6,8 @@ const todoCtrl = require('./../controllers/todo.controller');
 todoRouter.post('/', todoCtrl.createTodo);
 todoRouter.get('/', todoCtrl.getTodos)
 todoRouter.get('/:id', todoCtrl.getTodoById)
+todoRouter.delete('/:id', todoCtrl.getTodoById)
+todoRouter.put('/:id', todoCtrl.getTodoById)
+
 
 module.exports = todoRouter;
